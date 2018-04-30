ed7553864d1ed00e04c453a056154962d12c2504
Alex Calaca <alexcalaca@gmail.com>
2018-04-30 04:59:40 +0000

    Updated link for documents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
-  template: `
+  templateUrl: './app.component.html',
+  // template: `
   
-  <h1>Navigation</h1>
-  <router-outlet></router-outlet>
+  // <h1>Navigation</h1>
+  // <router-outlet></router-outlet>
   
-  `,
+  // `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
