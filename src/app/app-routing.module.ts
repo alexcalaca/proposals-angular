2e6ff3583dd2d37bae98514013a9ea98f081d178
Alex Calaca <alexcalaca@gmail.com>
2018-04-30 12:20:59 +0000

    Added ProposalList route to the app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import {  DocumentsComponent } from './documents/documents.component';
+import {  ProposalListComponent } from './proposal/proposal-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'documents', component: DocumentsComponent },
+  { path: 'proposals', component: ProposalListComponent },
 ]
 
 @NgModule({
