d32cf81636b80b1de2839d3e10228d6c5023d6ff
Alex Calaca <alexcalaca@gmail.com>
2018-04-30 12:18:53 +0000

    Added ProposalList component to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {  DocumentsComponent } from './documents/documents.component';
-
+import { DocumentsComponent } from './documents/documents.component';
+import { ProposalListComponent } from './proposal/proposal-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DocumentsComponent
+    DocumentsComponent,
+    ProposalListComponent
   ],
   imports: [
     BrowserModule,
