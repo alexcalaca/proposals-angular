4460b61b7e010c5e05531c625a62924c0b72725d
Alex Calaca <alexcalaca@gmail.com>
2018-05-01 03:33:50 +0000

    Updated app.module.ts file with new FormsModule import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { ProposalListComponent } from './proposal/proposal-list.component';
 import { ProposalNewComponent } from './proposal/proposal-new.component';
+
 
 
 @NgModule({
@@ -16,7 +19,8 @@ import { ProposalNewComponent } from './proposal/proposal-new.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
